de3ccf0ea9ab399d65b43095028a725475ec0775
美野航佑 <162241498+min0mi@users.noreply.github.com>
2026-09-07 07:41:19 +0900

    fix: include practice page in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,6 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   const routes = [
     '',
+    '/practice',
     '/practice/speed',
     '/practice/standard',
     '/practice/matching',
